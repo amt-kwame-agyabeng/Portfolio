4fd7b8e9c3d6701cca5a23c69ff4e43489c3e462
amt-kwame-agyabeng <kwameagyabeng63@gmail.com>
2024-09-06 23:25:24 +0000

    styled header, homepage features

--- my-portfolio/src/components/homePage.jsx
+++ my-portfolio/src/components/homePage.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 
 const HomePage = () => {
+  const handleResumeClick = () => {
+    const resumeLink =
+      "https://drive.google.com/file/d/1YdBosHeUvtQZd6mQQgR2ZbsVCdE47SpR/view?usp=sharing";
+    window.location.href = resumeLink;
+  };
 
-const handleResumeClick = () => {
-  const resumeLink = 'https://drive.google.com/file/d/1YdBosHeUvtQZd6mQQgR2ZbsVCdE47SpR/view?usp=sharing';
-  window.location.href = resumeLink;
+  const handleGitClick = () => {
+    const GitLink = "https://github.com/amt-kwame-agyabeng";
+    window.location.href = GitLink;
+  };
 
+  const handleLinkedinClick = () => {
+    const linkedinLink =
+      "https://www.linkedin.com/in/kwame-agyabeng-8603b3164/";
+    window.location.href = linkedinLink;
+  };
 
-}
-
-
+  const handleXClick = () => {
+    const XLink = "https://x.com/___nanakwame";
+    window.location.href = XLink;
+  };
 
   return (
     <div>
       <nav className="flex justify-between items-center  text-black">
-        <header className="text-3xl font-regular">DEV.KA</header>
+        <header className="text-5xl">🧑🏽‍💻</header>
         <div className="flex gap-10 items-center font-regular">
           <a href="#projects" className="hover:underline ">
             Projects
@@ -28,22 +40,38 @@ const handleResumeClick = () => {
           >
             Blog
           </a>
-          <button className="text-white bg-black border rounded-lg px-3 py-2 hover:bg-gray-700" onClick={handleResumeClick}>
+          <button
+            className="text-white bg-black border rounded-lg px-3 py-2 hover:bg-gray-700"
+            onClick={handleResumeClick}
+          >
             Resume
           </button>
         </div>
       </nav>
-      <h1 className="text-5xl font-bold mt-14">Frontend Developer</h1>
-      <h1 className="text-5xl font-bold text-gray-400">& Cloud Practitioner</h1>
-      <p className="mt-7 mb-4 font-regular tracking-wide leading-relaxed">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br></br>sed do
-        eiusmod tempor incididunt ut labore <br></br>et dolore magna aliqua. Ut
-        enim ad minim veniam, <br></br>quis nostrud exercitation ullamco laboris
-        nisi ut
-      </p>
-      <div className="flex items-center gap-2 mt-5 mb-5">
-        <p className="font-regular ">See More About Me</p>
+      <div className="flex items-center gap-2 mt-9">
         <svg
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 1024 1024"
+          height="3em"
+          width="2em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M904 476H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8z"></path>
+        </svg>
+        <div className="flex flex-row items-center">
+          <p className="font-extralight text-xl">Hello I'm</p>
+          <span role="img" aria-label="victoryhand">
+          ✌🏽
+          </span>
+        </div>
+      </div>
+      <p className="font-light text-6xl">KWAME</p>
+      <p className="font-light text-6xl">AGYABENG</p>
+      <p className="font-light mt-5">Frontend Developer {""} | {""} Cloud Practitioner Based in Ghana 🇬🇭</p>
+
+        <button className="border-2 border-gray-900 px-4 py-1 rounded-lg  flex items-center gap- mt-5 ">See More <svg
           stroke="currentColor"
           fill="currentColor"
           stroke-width="0"
@@ -53,18 +81,21 @@ const handleResumeClick = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path d="M295.6 163.7c-5.1 5-5.1 13.3-.1 18.4l60.8 60.9H124.9c-7.1 0-12.9 5.8-12.9 13s5.8 13 12.9 13h231.3l-60.8 60.9c-5 5.1-4.9 13.3.1 18.4 5.1 5 13.2 5 18.3-.1l82.4-83c1.1-1.2 2-2.5 2.7-4.1.7-1.6 1-3.3 1-5 0-3.4-1.3-6.6-3.7-9.1l-82.4-83c-4.9-5.2-13.1-5.3-18.2-.3z"></path>
-        </svg>
-      </div>
+        </svg></button>
+        
+      
 
-      <div className="flex gap-10 items-center">
+      <div className="flex gap-10 items-center mt-10">
         <svg
           stroke="currentColor"
           fill="currentColor"
           stroke-width="0"
           viewBox="0 0 1024 1024"
-          height="2em"
-          width="2em"
+          height="1.8em"
+          width="1.8em"
           xmlns="http://www.w3.org/2000/svg"
+          onClick={handleGitClick}
+          style={{ cursor: "pointer" }}
         >
           <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path>
         </svg>
@@ -74,9 +105,11 @@ const handleResumeClick = () => {
           fill="currentColor"
           stroke-width="0"
           viewBox="0 0 24 24"
-          height="2em"
-          width="2em"
+          height="1.8em"
+          width="1.8em"
           xmlns="http://www.w3.org/2000/svg"
+          onClick={handleLinkedinClick}
+          style={{ cursor: "pointer" }}
         >
           <path
             fill-rule="evenodd"
@@ -88,9 +121,11 @@ const handleResumeClick = () => {
           fill="currentColor"
           stroke-width="0"
           viewBox="0 0 1024 1024"
-          height="2em"
-          width="2em"
+          height="1.8em"
+          width="1.8em"
           xmlns="http://www.w3.org/2000/svg"
+          onClick={handleXClick}
+          style={{ cursor: "pointer" }}
         >
           <path d="M928 254.3c-30.6 13.2-63.9 22.7-98.2 26.4a170.1 170.1 0 0 0 75-94 336.64 336.64 0 0 1-108.2 41.2A170.1 170.1 0 0 0 672 174c-94.5 0-170.5 76.6-170.5 170.6 0 13.2 1.6 26.4 4.2 39.1-141.5-7.4-267.7-75-351.6-178.5a169.32 169.32 0 0 0-23.2 86.1c0 59.2 30.1 111.4 76 142.1a172 172 0 0 1-77.1-21.7v2.1c0 82.9 58.6 151.6 136.7 167.4a180.6 180.6 0 0 1-44.9 5.8c-11.1 0-21.6-1.1-32.2-2.6C211 652 273.9 701.1 348.8 702.7c-58.6 45.9-132 72.9-211.7 72.9-14.3 0-27.5-.5-41.2-2.1C171.5 822 261.2 850 357.8 850 671.4 850 843 590.2 843 364.7c0-7.4 0-14.8-.5-22.2 33.2-24.3 62.3-54.4 85.5-88.2z"></path>
         </svg>
@@ -102,7 +137,10 @@ const handleResumeClick = () => {
             className="border border-gray-300 p-2 rounded-lg bg-[#DDEEF6] flex-1 hover:shadow-xl transition-shadow duration-300 flex items-center justify-between"
             style={{ backgroundColor: "rgba(221, 238, 246, 0.4)" }}
           >
-            <p className="cursor-default font-regular"> Personal Portfolio website</p>
+            <p className="cursor-default font-regular">
+              {" "}
+              Personal Portfolio website
+            </p>
             <svg
               stroke="currentColor"
               fill="none"
@@ -125,7 +163,9 @@ const handleResumeClick = () => {
             className="border border-gray-300 p-2 rounded-lg bg-[#DDEEF6] flex-1 hover:shadow-xl transition-shadow duration-300 flex items-center justify-between"
             style={{ backgroundColor: "rgba(221, 238, 246, 0.4)" }}
           >
-            <p className="cursor-default font-regular">Currently Learning ~ AWS Restart Program</p>
+            <p className="cursor-default font-regular">
+              Currently Learning ~ AWS Restart Program
+            </p>
             <svg
               stroke="currentColor"
               fill="none"
@@ -145,25 +185,19 @@ const handleResumeClick = () => {
           </div>
         </div>
         <div className="flex flex-row gap-10 items-center text-center font-regular">
-        <div className=" border border-gray-300 p-3 rounded-lg mt-6">
-         <p>Project Name</p>
-         <p >Project Date</p>
-         
+          <div className=" border border-gray-300 p-3 rounded-lg mt-6">
+            <p>Project Name</p>
+            <p>Project Date</p>
+          </div>
+          <div className=" border border-gray-300 p-3 rounded-lg mt-6">
+            <p>Project Name</p>
+            <p>Project Date</p>
+          </div>
+          <div className=" border border-gray-300 p-3 rounded-lg mt-6">
+            <p>Project Name</p>
+            <p>Project Date</p>
+          </div>
         </div>
-        <div className=" border border-gray-300 p-3 rounded-lg mt-6">
-        <p >Project Name</p>
-        <p>Project Date</p>
-        
-       
-        </div>
-        <div className=" border border-gray-300 p-3 rounded-lg mt-6">
-        <p>Project Name</p>
-        <p>Project Date</p>
-       
-       
-        </div>
-        </div>
-
       </div>
 
       <div className="skill">
