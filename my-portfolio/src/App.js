3a93f0a59bcf5ff1d6b94d748ac51567ac86851f
amt-kwame-agyabeng <kwameagyabeng63@gmail.com>
2024-09-06 23:27:17 +0000

    modify dependencies

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -5,10 +5,12 @@ import HomePage from './components/homePage';
 
 
 
+
 function App() {
   return (
     <div className="App px-12 pt-10">
-      <HomePage/>
+     <HomePage/>
+    
     </div>
   );
 }
--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        regular: ['Regular'],
+        medium: ['Medium'],
+        bold: ['Bold'],
+        semibold: ['Semibold'],
+        light: ['Light'],
+        
+        
+
+
+      }
+    },
   },
   plugins: [],
 }
