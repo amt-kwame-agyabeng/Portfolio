5c1a4a46933ae544f82def76e54cf0286098afbe
amt-kwame-agyabeng <kwameagyabeng63@gmail.com>
2024-09-10 02:48:34 +0000

    dark mode theme

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import HomePage from './components/homePage';
 
@@ -5,11 +6,19 @@ import HomePage from './components/homePage';
 
 
 
-
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+  
+
+  
+
   return (
-    <div className="App px-12 pt-10">
-     <HomePage/>
+    <div className={`App px-12 pt-10 ${darkMode ? 'dark' : 'light'}`}> 
+     <HomePage onToggle={toggleDarkMode}/>
     
     </div>
   );
