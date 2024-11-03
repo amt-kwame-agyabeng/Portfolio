ba15196bf073ec5962851a469ae3c9757ad072a8
amt-kwame-agyabeng <kwameagyabeng63@gmail.com>
2024-11-03 00:05:20 +0000

    tailwind.config.js file re-edited

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,23 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
   theme: {
     extend: {
       fontFamily: {
-        regular: ['Regular'],
-        medium: ['Medium'],
-        bold: ['Bold'],
-        semibold: ['Semibold'],
-        light: ['Light'],
-        
-        
-
-
-      }
+        regular: ["Regular"],
+        medium: ["Medium"],
+        bold: ["Bold"],
+        semibold: ["Semibold"],
+        light: ["Light"],
+      },
     },
   },
   plugins: [],
-}
-
+};
